9edf2f50df076019b97bf4a0628083ab665cb520
smhrd <smhrd@192.168.219.49>
2024-11-04 11:24:57 +0900

    윤동빈 커밋

---
+++ ospaceproject/src/main/java/com/smhrd/model/Used_ProductsDAO.java
@@ -0,0 +1,37 @@
+package com.smhrd.model;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.database.SqlSessionManager;
+
+public class Used_ProductsDAO {
+
+	SqlSessionFactory factory = SqlSessionManager.getSqlSessionFactory();
+
+	public int insert(Used_Products usedproduct) {
+
+		SqlSession sqlSession = factory.openSession(true);
+		int result = sqlSession.insert("Mapper.write", usedproduct);
+
+		sqlSession.close();
+		return result;
+	}
+
+	public List<Used_Products> getu_prod() {
+	    SqlSession session = factory.openSession(true);
+	    List<Used_Products> result = session.selectList("Mapper.getu_prod");
+	    session.close();
+	    return result;
+	}
+
+	public Used_Products getu_prod(int used_id) {
+	    SqlSession session = factory.openSession(true);
+	    Used_Products result = session.selectOne("Mapper.get_single_prod", used_id);
+	    session.close();
+	    return result;
+	}
+	
+}
