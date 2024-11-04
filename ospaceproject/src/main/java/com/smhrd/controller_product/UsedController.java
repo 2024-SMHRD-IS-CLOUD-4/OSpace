75bbb3625bdea08d17a53f1f8fd075f6c2f9326e
smhrd <smhrd@192.168.219.248>
2024-11-04 23:19:54 +0900

    윤성 커밋

--- ospaceproject/src/main/java/com/smhrd/controller_product/UsedController.java
+++ ospaceproject/src/main/java/com/smhrd/controller_product/UsedController.java
@@ -14,42 +14,42 @@ import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import com.smhrd.model.Used_Products;
 import com.smhrd.model.Used_ProductsDAO;
 
-
 @WebServlet("/UsedController")
 public class UsedController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		request.setCharacterEncoding("UTF-8");
-		
+
 		ServletContext context = request.getServletContext();
 		String uploadpath = context.getRealPath("used");
-		
-		int maxSize = 5*1024*1024;
-		
-		MultipartRequest multi = new MultipartRequest(request, uploadpath, maxSize, "UTF-8", new DefaultFileRenamePolicy());
-		
+
+		int maxSize = 5 * 1024 * 1024;
+
+		MultipartRequest multi = new MultipartRequest(request, uploadpath, maxSize, "UTF-8",
+				new DefaultFileRenamePolicy());
+
 		String id = multi.getParameter("id");
 		String used_title = multi.getParameter("used_title");
 		String used_content = multi.getParameter("used_content");
 		int used_price = Integer.parseInt(multi.getParameter("used_price"));
 		String used_location = multi.getParameter("used_location");
 		String used_img = multi.getFilesystemName("used_img");
-		
-		Used_Products usedproduct = new Used_Products(id, used_title, used_content, used_price, used_location, used_img); 
-		
+
+		Used_Products usedproduct = new Used_Products(0, id, used_title, used_price, used_location, 0, used_img, null,
+				'n', used_content);
+
 		Used_ProductsDAO dao = new Used_ProductsDAO();
 		int result = dao.insert(usedproduct);
-		
-		 if(result >0) {
-		    	response.sendRedirect("Used_ProductsList.jsp");
-		    }else {
-		    	response.sendRedirect("UsedAdd.jsp");
-		    }
-		
-	
+
+		if (result > 0) {
+			response.sendRedirect("Used_ProductsList.jsp");
+		} else {
+			response.sendRedirect("UsedAdd.jsp");
+		}
+
 	}
 
 }
