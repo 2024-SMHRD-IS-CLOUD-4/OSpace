bfb78f0ceab51f25f4b208c8a13bf1ac497d906a
smhrd <smhrd@192.168.219.49>
2024-11-05 10:04:05 +0900

    커밋 완료

---
+++ ospaceproject/src/main/java/com/smhrd/model/ReviewsDAO.java
@@ -0,0 +1,32 @@
+package com.smhrd.model;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.database.SqlSessionManager;
+
+public class ReviewsDAO {
+
+	SqlSessionFactory factory = SqlSessionManager.getSqlSessionFactory();
+	
+	public int insert(Reviews review) {
+		
+		SqlSession sqlSession = factory.openSession(true);
+		int result = sqlSession.insert("ReviewMapper.review_insert", review);
+		
+		sqlSession.close();
+		return result;
+	}
+	
+	public List<Reviews> getreview(){
+		SqlSession session = factory.openSession(true);
+		List<Reviews> result = session.selectList("ReviewMapper.get_reviews");
+		session.close();
+	    return result;	
+	}
+	
+	
+	
+}
